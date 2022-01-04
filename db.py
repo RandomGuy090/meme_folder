9e8a5403a4ec24a87b6c9273c3f0469f6b57445e
RandomGuy090 <randomguy0090@gmail.com>
2022-01-04 11:45:02 +0100

    shasum collumn in Memes table, UniqueConstraint in Map_tags

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ import sys
 from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, UniqueConstraint
  
 Base = declarative_base()
  
@@ -14,6 +14,7 @@ class Memes(Base):
     path = Column(String(250), nullable=False)
     full_filename = Column(String(250), nullable=False)
     exists = Column(Boolean(), nullable=False)
+    shasum = Column(String(50), nullable=False)
 
 
  
@@ -24,17 +25,23 @@ class Tags(Base):
 
 
 
+
 class Map_tags(Base):
     __tablename__ = 'map_tags'
+    __table_args__ = (
+        UniqueConstraint('meme_id', 'tag_id'),
+        )
+    
     id = Column(Integer, primary_key=True)
     # meme_id = Column(Integer, nullable=False)
     # tag_id = Column(Integer, nullable=False)
 
-    meme_id = Column(Integer, ForeignKey("memes_table.id"))
-    tag_id = Column(Integer, ForeignKey("tag_table.id"))
+    meme_id = Column(Integer, ForeignKey("memes_table.id",onupdate="CASCADE", ondelete="CASCADE"), nullable=False)
+    tag_id = Column(Integer, ForeignKey("tag_table.id", onupdate="CASCADE", ondelete="CASCADE"), nullable=False)
 
     meme = relationship("Memes")
     tag = relationship("Tags")
+
 
 
 
@@ -61,3 +68,14 @@ if "-c" in sys.argv or "--create" in sys.argv:
 # WHERE tag_table.tag_name LIKE "kabaczek"
 # GROUP BY filename
 # ;
+
+
+# SELECT meme_id, filename, 
+#     group_concat(tag_name,' ~ ') AS tags_for_this_object 
+# FROM map_tags 
+# JOIN memes_table ON meme_id = memes_table.id
+# JOIN tag_table ON tag_id = tag_table.id
+# WHERE   map_tags.meme_id LIKE 784
+# GROUP BY meme_id
+# ;
+
