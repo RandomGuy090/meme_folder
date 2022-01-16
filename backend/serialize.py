a0a634dd3e68cc4f38d23da61825ac53384aaf9f
RandomGuy090 <randomguy0090@gmail.com>
2022-01-16 12:34:37 +0100

    serialization moved, custom serializer class

--- backend/serialize.py
+++ backend/serialize.py
@@ -1,4 +1,9 @@
 
+from flask_marshmallow import Marshmallow 
+import os, json
+from var import * 
+from db import *
+
 
 class Serialise_data(object):
 
@@ -36,3 +41,78 @@ class Serialise_data(object):
 			
 
 
+ma = Marshmallow(app)
+
+
+class Removed_files(ma.Schema):
+	class Meta:
+  		model = Memes
+  		fields = ('id', 'filename', 'path', 'full_filename', 'exists', "shasum")
+
+removed_files = Removed_files(many=True)
+
+class New_files(ma.Schema):
+	class Meta:
+		model = Memes
+		fields = ('id', 'filename', 'path', 'full_filename', 'exists', "shasum", "tags")
+	
+	tags = ma.List(ma.String(), required=False)
+
+new_files = New_files(many=True)
+
+
+class Moved_files(ma.Schema):
+	class Meta:
+		model = New_files_table
+		fields = ( "meme_new", "meme_old")
+	
+
+	meme_new = ma.Nested(New_files)
+	meme_old = ma.Nested(New_files)
+
+moved_files = Moved_files(many=True)
+
+
+
+class All_memes():
+
+	class Meta:
+		# model = Memes
+		strict = True
+		fields = ('id', 'filename', 'path', 'full_filename', 'exists', "shasum", "tags")
+
+	# group_concat_1 = ma.List(ma.String(), required=True)
+		
+	def __init__(self,many=False):
+		self.obj = None
+		self.ret = list()
+		print(self.Meta.fields)
+
+	def dump(self, obj):
+		self.obj = obj
+		return self.serialize()
+	
+	def serialize(self):
+		for elem in self.obj:
+			x = dict()
+			for col in self.Meta.fields:
+				val = self.get_values(element=elem, index=self.Meta.fields.index(col), separator="~")
+				x[col] = val
+
+			self.ret.append(x)
+		return self.parse_json()
+		
+		# print("____?")
+		# print(self.ret)
+	def parse_json(self):
+		return self.ret
+		# return json.dumps(self.ret)
+
+	def get_values(self, element=None, index=None, separator=""):
+		column = self.Meta.fields[index]
+		if column == "tags":
+			return element[index].rsplit(separator)
+		return element[index]
+
+
+all_memes = All_memes(many=True)
