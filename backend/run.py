f8532ec7621f9c1c17dda91d8aeb77cc7a7a1b9d
RandomGuy090 <randomguy0090@gmail.com>
2022-02-23 23:53:21 +0100

    multithreading connecting http and flask/api server in one script

--- backend/run.py
+++ backend/run.py
@@ -1,7 +1,14 @@
 # from app import app
 from routing import app
 from http_server import run_http_server
+from threading import Thread
+from app import *
 
 if __name__ == '__main__':
+
+  # flask_thread = Thread(target = app.run, kwargs={"debug":True, "host":"127.0.0.1", "port":12345}).start()
+  http_thread = Thread(target = run_http_server).start()
+  app.run( host="127.0.0.1", port=PORT_FLASK)
+
   # run_http_server()
-  app.run(debug=True)
+  
