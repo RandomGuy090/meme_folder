3f1eb88089619e10f1369a042eaf1d3afbb72311
RandomGuy090 <randomguy0090@gmail.com>
2022-02-23 23:52:38 +0100

    ports

--- backend/var.py
+++ backend/var.py
@@ -3,7 +3,8 @@ from flask_cors import CORS, cross_origin
 
 PATHDIR = "/media/randomguy90/cheny/"
 DB_NAME = "sqlalchemy_example"
-PORT = 5000
+PORT_FLASK = 5000
+PORT_IMGS = 5001
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
