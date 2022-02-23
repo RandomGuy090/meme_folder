d15ff2ad47ec0352af411b019fbc21681217fde1
RandomGuy090 <randomguy0090@gmail.com>
2022-02-23 23:37:56 +0100

    new variables

--- backend/var.py
+++ backend/var.py
@@ -3,6 +3,8 @@ from flask_cors import CORS, cross_origin
 
 PATHDIR = "/media/randomguy90/cheny/"
 DB_NAME = "sqlalchemy_example"
+PORT = 5000
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app, resources=r'/*')
